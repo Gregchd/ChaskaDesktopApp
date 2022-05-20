3bd5d5aebf703c89779299e3f51cf8df488e6ede
mjarafalconi <mjarafalconi@gmail.com>
2022-05-20 01:52:59 -0500

    Ajustes: se agrego comentarios.

--- src/views/header.js
+++ src/views/header.js
@@ -13,12 +13,13 @@ function closeWindow() {
     ipcRenderer.send('closeWindow');
 }
 
+// Event Listeners
 document.getElementById('min-button').addEventListener('click', minimizeWindow);
 document.getElementById('max-button').addEventListener('click', maximizeWindow);
 document.getElementById('restore-button').addEventListener('click', unmaximizeWindow);
 document.getElementById('close-button').addEventListener('click', closeWindow);
 
-
+// TODO: MAKE THE MIDDLE BUTTON TOGGLE BETWEEN MAXIMIZE AND UNMAXIMAZE
 //  function handleWindowControls() {
 //      // Toggle maximise/restore buttons when maximisation/unmaximisation occurs
 //      toggleMaxRestoreButtons();
