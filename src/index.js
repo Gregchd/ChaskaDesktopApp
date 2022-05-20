5ae602edacce206822a880afa2c5e702d7aa6aea
mjarafalconi <mjarafalconi@gmail.com>
2022-05-20 00:17:16 -0500

    Ajustes: Arreglo de ubicaciones, formatos y nombres de archivos.

--- src/index.js
+++ src/index.js
@@ -1,66 +1,33 @@
 //Librerias de electron
-const { app, BrowserWindow } = require("electron");
+const {
+  app,
+  BrowserWindow
+} = require("electron");
 //otras librerias
 const url = require("url");
 const path = require("path");
 
-//require("./main");
-
 let mainWindow;
 
-//Inicializando ventana de la app y configs
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 900,
-    height: 800,
-    frame: false, //Set the browser window without upper bar
-    backgroundColor: '#FFF',
+    height: 900,
     webPreferences: {
       contextIsolation: false,
       enableRemoteModule: true,
       nodeIntegration: true
     },
-    icon: __dirname + "./icons/chaska.ico"
+    frame: false
   });
-  mainWindow.loadFile("views/index.html");
-  // mainWindow.loadURL(
-  //   url.format({
-  //     pathname: path.join(__dirname, "views/index.html"),
-  //     protocol: "file",
-  //     slashes: true,
-  //   })
-  // );
+
+  mainWindow.loadFile("index.html");
+
+  mainWindow.maximize();
+
   mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
-});
+  });
 }
 
 app.on("ready", createWindow);
-
-// app.on("ready", () => {
-//   mainWindow = new BrowserWindow({
-//     width: 900,
-//     height: 800,
-//     frame: false, //Set the browser window without upper bar
-//     webPreferences: {
-//       nodeIntegration: true,
-//       contextIsolation: false,
-//       enableRemoteModule: true
-//     },
-//     icon: __dirname + "./ico/chaska.ico"
-//   });
-
-//   mainWindow.loadURL(
-//     url.format({
-//       pathname: path.join(__dirname, "views/index.html"),
-//       protocol: "file",
-//       slashes: true,
-//     })
-//   );
-// });
-
-
-
--- src/views/home.js
+++ src/views/home.js
@@ -1,5 +1,5 @@
 const { SerialPort } = require("serialport");
-require("../database");
+require("../database"); //src\views\database.js
 const { ipcRenderer } = require("electron");
 const { ReadlineParser } = require("@serialport/parser-readline");
 //const port = new SerialPort({ path: "COM4", baudRate: 9600 });
@@ -161,55 +161,58 @@ function serial_connection(correct_path){
   });
 }
 
-// HANDLING CLOSE ACTIONS
-const remote = require('electron');
-const win = remote.getCurrentWindow(); /* Note this is different to the
-html global `window` variable */
+// const {remote} = require('electron');
+// const win = remote.getCurrentWindow();
+// console.log(remote);
+// // HANDLING CLOSE ACTIONS
+// const remote = require('electron');
+// const win = remote.getCurrentWindow(); /* Note this is different to the
+// html global `window` variable */
 
-// When document has loaded, initialise
-document.onreadystatechange = (event) => {
-    if (document.readyState == "complete") {
-        handleWindowControls();
+// // When document has loaded, initialise
+// document.onreadystatechange = (event) => {
+//     if (document.readyState == "complete") {
+//         handleWindowControls();
 
-        document.getElementById('electron-ver').innerHTML = `${process.versions.electron}`
-    }
-};
+//         document.getElementById('electron-ver').innerHTML = `${process.versions.electron}`
+//     }
+// };
 
-window.onbeforeunload = (event) => {
-    /* If window is reloaded, remove win event listeners
-    (DOM element listeners get auto garbage collected but not
-    Electron win listeners as the win is not dereferenced unless closed) */
-    win.removeAllListeners();
-}
+// window.onbeforeunload = (event) => {
+//     /* If window is reloaded, remove win event listeners
+//     (DOM element listeners get auto garbage collected but not
+//     Electron win listeners as the win is not dereferenced unless closed) */
+//     win.removeAllListeners();
+// }
 
-function handleWindowControls() {
-    // Make minimise/maximise/restore/close buttons work when they are clicked
-    document.getElementById('min-button').addEventListener("click", event => {
-        win.minimize();
-    });
+// function handleWindowControls() {
+//     // Make minimise/maximise/restore/close buttons work when they are clicked
+//     document.getElementById('min-button').addEventListener("click", event => {
+//         win.minimize();
+//     });
 
-    document.getElementById('max-button').addEventListener("click", event => {
-        win.maximize();
-    });
+//     document.getElementById('max-button').addEventListener("click", event => {
+//         win.maximize();
+//     });
 
-    document.getElementById('restore-button').addEventListener("click", event => {
-        win.unmaximize();
-    });
+//     document.getElementById('restore-button').addEventListener("click", event => {
+//         win.unmaximize();
+//     });
 
-    document.getElementById('close-button').addEventListener("click", event => {
-        win.close();
-    });
+//     document.getElementById('close-button').addEventListener("click", event => {
+//         win.close();
+//     });
 
-    // Toggle maximise/restore buttons when maximisation/unmaximisation occurs
-    toggleMaxRestoreButtons();
-    win.on('maximize', toggleMaxRestoreButtons);
-    win.on('unmaximize', toggleMaxRestoreButtons);
+//     // Toggle maximise/restore buttons when maximisation/unmaximisation occurs
+//     toggleMaxRestoreButtons();
+//     win.on('maximize', toggleMaxRestoreButtons);
+//     win.on('unmaximize', toggleMaxRestoreButtons);
 
-    function toggleMaxRestoreButtons() {
-        if (win.isMaximized()) {
-            document.body.classList.add('maximized');
-        } else {
-            document.body.classList.remove('maximized');
-        }
-    }
-}
+//     function toggleMaxRestoreButtons() {
+//         if (win.isMaximized()) {
+//             document.body.classList.add('maximized');
+//         } else {
+//             document.body.classList.remove('maximized');
+//         }
+//     }
+// }
